7e6e4da94c8903c072a275a89c9ae701ed3f1a3c
ChandlerF <63360113+ChandlerF@users.noreply.github.com>
2022-11-04 19:01:43 -0500

    TurretLineOfSight

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
+    public bool CanShoot = true, CanRotate = true;
     private List<Transform> _enemyList = new List<Transform>();
     [SerializeField] private float _speed = 200f;
 
 
 
     [SerializeField] private int _amountOfAmmo;
-    [SerializeField] private float _startTimeBetweenShots;
+    [SerializeField] private float _startTimeBetweenShots, _accuracyBeforeShooting;
     private float _timeBetweenShots;
 
     [SerializeField] private GameObject _bullet;
     [SerializeField] private Transform _shootPoint;
+    private Quaternion _targetRotation;
+
 
 
 
@@ -32,13 +35,21 @@ public class Turret : MonoBehaviour
 
     private void Update()
     {
+        //If enemies, rotate
         if (_enemyList.Count > 0)
             RotateTowardsEnemy();
 
+        //If ammo and time on timer
         if (_amountOfAmmo > 0 && _timeBetweenShots > 0)
             _timeBetweenShots -= Time.deltaTime;
 
-        if (_enemyList.Count > 0 && _timeBetweenShots <= 0 && _amountOfAmmo > 0)
+
+        //If enemies, no Timer, Has Ammo, and is aiming at target
+        if (_enemyList.Count > 0 &&
+            _timeBetweenShots <= 0 &&
+            _amountOfAmmo > 0 &&
+            Quaternion.Angle(transform.rotation, _targetRotation) < _accuracyBeforeShooting)
+
             Shoot();
     }
 
@@ -64,6 +75,7 @@ public class Turret : MonoBehaviour
 
     private void RotateTowardsEnemy()
     {
+
         Vector3 _target = _enemyList[0].position;
         _target.z = 0;
 
@@ -72,8 +84,12 @@ public class Turret : MonoBehaviour
 
         float _angle = Mathf.Atan2(_target.y, _target.x) * Mathf.Rad2Deg;
 
-        Quaternion _targetRotation = Quaternion.Euler(new Vector3(0, 0, _angle - 90f));
+        _targetRotation = Quaternion.Euler(new Vector3(0, 0, _angle - 90f));
 
+
+
+
+        if (!CanRotate || !HasLineOfSight()) return;
         transform.rotation = Quaternion.RotateTowards(transform.rotation, _targetRotation, _speed * Time.deltaTime);
     }
 
@@ -81,10 +97,22 @@ public class Turret : MonoBehaviour
 
     private void Shoot()
     {
+        if (!CanShoot) return;
+
         GameObject _spawnedBullet = Instantiate(_bullet, _shootPoint.position, Quaternion.identity);
         _spawnedBullet.GetComponent<Rigidbody2D>().AddForce(transform.up * 30f, ForceMode2D.Impulse);
         _timeBetweenShots = _startTimeBetweenShots;
         _amountOfAmmo --;
+    }
+
+
+    private bool HasLineOfSight()
+    {
+        if (_enemyList.Count == 0) return false;
+
+        //Debug.DrawRay(transform.position, _enemyList[0].transform.position - transform.position, Color.red);
+        return Physics2D.Raycast(transform.position, (_enemyList[0].transform.position - transform.position).normalized).transform.TryGetComponent<Enemy>(out Enemy _enemyScript);
+        //RaycastHit2D _hit = Physics2D.Raycast(transform.position, (_enemyList[0].transform.position - transform.position).normalized);
     }
 
     //Raycast to see if Turret has line of sight
