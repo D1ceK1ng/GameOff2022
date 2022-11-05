73b70cd4d2b4f5882644125e8fca8015f27a0f64
ChandlerF <63360113+ChandlerF@users.noreply.github.com>
2022-11-04 21:27:26 -0500

    AimPrediction

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -6,7 +6,7 @@ public class Turret : MonoBehaviour
 {
     public bool CanShoot = true, CanRotate = true;
     private List<Transform> _enemyList = new List<Transform>();
-    [SerializeField] private float _speed = 200f;
+    [SerializeField] private float _rotationSpeed = 200f, _bulletSpeed = 30f;
 
 
 
@@ -82,7 +82,14 @@ public class Turret : MonoBehaviour
         _target.x -= transform.position.x;
         _target.y -= transform.position.y;
 
-        float _angle = Mathf.Atan2(_target.y, _target.x) * Mathf.Rad2Deg;
+        float _angle;
+
+        if (InterceptionDirection(_enemyList[0].position, transform.position, _enemyList[0].GetComponent<Rigidbody2D>().velocity, _bulletSpeed, out var direction))
+            _angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        
+        else
+            _angle = Mathf.Atan2(_target.y, _target.x) * Mathf.Rad2Deg;
+        
 
         _targetRotation = Quaternion.Euler(new Vector3(0, 0, _angle - 90f));
 
@@ -90,7 +97,7 @@ public class Turret : MonoBehaviour
 
 
         if (!CanRotate || !HasLineOfSight()) return;
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, _targetRotation, _speed * Time.deltaTime);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, _targetRotation, _rotationSpeed * Time.deltaTime);
     }
 
 
@@ -100,7 +107,8 @@ public class Turret : MonoBehaviour
         if (!CanShoot) return;
 
         GameObject _spawnedBullet = Instantiate(_bullet, _shootPoint.position, Quaternion.identity);
-        _spawnedBullet.GetComponent<Rigidbody2D>().AddForce(transform.up * 30f, ForceMode2D.Impulse);
+
+        _spawnedBullet.GetComponent<Rigidbody2D>().AddForce(transform.up * _bulletSpeed, ForceMode2D.Impulse);
         _timeBetweenShots = _startTimeBetweenShots;
         _amountOfAmmo --;
     }
@@ -115,8 +123,67 @@ public class Turret : MonoBehaviour
         //RaycastHit2D _hit = Physics2D.Raycast(transform.position, (_enemyList[0].transform.position - transform.position).normalized);
     }
 
-    //Raycast to see if Turret has line of sight
-    //Wait to fire until aiming at it
+
+
+
+    private bool InterceptionDirection(Vector2 a, Vector2 b, Vector2 vA, float sB, out Vector2 result)
+    {
+        var aToB = b - a;
+        var dC = aToB.magnitude;
+        var alpha = Vector2.Angle(aToB, vA) * Mathf.Deg2Rad;
+        var sA = vA.magnitude;
+        var r = sA / sB;
+
+        if(SolveQuadratic(1 - r * r, 2 * r * dC * Mathf.Cos(alpha), -(dC * dC), out var root1, out var root2) == 0)
+        {
+            //No valid Solutions
+            result = Vector2.zero;
+            return false;
+        }
+
+        var dA = Mathf.Max(root1, root2);
+        var t = dA / sB;
+        var c = a + vA * t;
+        result = (c - b).normalized;
+        return true;
+    }
+
+
+
+    private static int SolveQuadratic(float a, float b, float c, out float root1, out float root2)
+    {
+        var discriminant = b * b - 4 * a * c;
+        if(discriminant < 0)
+        {
+            root1 = Mathf.Infinity;
+            root2 = -root1;
+            return 0;
+        }
+
+        root1 = (-b + Mathf.Sqrt(discriminant)) / (2 * a);
+        root2 = (-b - Mathf.Sqrt(discriminant)) / (2 * a);
+       
+        return discriminant > 0 ? 2 : 1;
+    }
+
     //Make bullets follow
     //Recoil
 }
+
+
+
+
+/*
+        float _angle;
+
+        if (InterceptionDirection(_enemyList[0].position, transform.position, _enemyList[0].GetComponent<Rigidbody2D>().velocity, _bulletSpeed, out var direction))
+        {
+            _angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+            Debug.Log("new");
+        }
+        else
+        {
+            _angle = Mathf.Atan2(_target.y, _target.x) * Mathf.Rad2Deg;
+        }
+*/
